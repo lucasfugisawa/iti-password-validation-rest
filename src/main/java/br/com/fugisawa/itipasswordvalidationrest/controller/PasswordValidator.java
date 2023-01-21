31ff0cdb0319ec42a35e91d52177ba40aaeae037
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-01-21 16:52:06 -0300

    Added OpenAPI REST documentation endpoint and Swagger UI.

--- src/main/java/br/com/fugisawa/itipasswordvalidationrest/controller/PasswordValidator.java
+++ src/main/java/br/com/fugisawa/itipasswordvalidationrest/controller/PasswordValidator.java
@@ -1,5 +1,9 @@
 package br.com.fugisawa.itipasswordvalidationrest.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,6 +16,9 @@ public class PasswordValidator {
         this.passwordValidator = passwordValidator;
     }
 
+    @Operation(summary = "Checks if a password is valid for ITI digital services.")
+    @Parameter(in = ParameterIn.PATH, name = "password", description = "The password to be checked.")
+    @ApiResponse(responseCode = "200", description = "Returns true if password is valid. Otherwise returns false.")
     @GetMapping("/validate/{password}")
     @ResponseBody
     public Boolean validatePassword(@PathVariable(name = "password") String password) {
