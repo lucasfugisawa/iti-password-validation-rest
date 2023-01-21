607a071ab20861ce1cd2e4394ee3d41d88a6521d
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-01-21 16:49:15 -0300

    Spring application main class.

---
+++ src/main/java/br/com/fugisawa/itipasswordvalidationrest/ItiPasswordValidationRestApplication.java
@@ -0,0 +1,12 @@
+package br.com.fugisawa.itipasswordvalidationrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItiPasswordValidationRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItiPasswordValidationRestApplication.class, args);
+    }
+}
