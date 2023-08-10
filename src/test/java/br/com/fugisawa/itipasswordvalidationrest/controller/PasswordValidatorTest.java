cd83a425b1f4e3b6532ecfc8137ef24c4fddc58c
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-08-10 13:55:19 -0300

    Automated tests for Service and Controller.

--- src/test/java/br/com/fugisawa/itipasswordvalidationrest/controller/PasswordValidatorTest.java
+++ src/test/java/br/com/fugisawa/itipasswordvalidationrest/controller/PasswordValidatorTest.java
@@ -1,17 +1,52 @@
 package br.com.fugisawa.itipasswordvalidationrest.controller;
 
+import br.com.fugisawa.itipasswordvalidationrest.service.PasswordValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 class PasswordValidatorTest {
 
+    @Mock
+    private PasswordValidator passwordValidator;
+
+    private MockMvc mockMvc;
+
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
+        var passwordValidatorController = new br.com.fugisawa.itipasswordvalidationrest.controller.PasswordValidator(passwordValidator);
+        mockMvc = MockMvcBuilders.standaloneSetup(passwordValidatorController).build();
     }
 
     @Test
-    void validatePassword() {
+    void validatePassword_ValidPassword_ReturnsTrue() throws Exception {
+        String validPassword = "StrongPassword123!";
+        when(passwordValidator.validate(validPassword)).thenReturn(true);
+
+        mockMvc.perform(get("/password/validate/{password}", validPassword)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+    @Test
+    void validatePassword_InvalidPassword_ReturnsFalse() throws Exception {
+        String invalidPassword = "WeakPassword";
+        when(passwordValidator.validate(invalidPassword)).thenReturn(false);
+
+        mockMvc.perform(get("/password/validate/{password}", invalidPassword)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("false"));
     }
 }
--- src/test/java/br/com/fugisawa/itipasswordvalidationrest/service/DefaultPasswordValidatorTest.java
+++ src/test/java/br/com/fugisawa/itipasswordvalidationrest/service/DefaultPasswordValidatorTest.java
@@ -1,17 +1,45 @@
 package br.com.fugisawa.itipasswordvalidationrest.service;
 
+import br.com.fugisawa.itipasswordvalidationrest.domain.PasswordValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 class DefaultPasswordValidatorTest {
 
+    @Mock
+    private PasswordValidator passwordValidator;
+
+    private DefaultPasswordValidator defaultPasswordValidator;
+
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
+        defaultPasswordValidator = new DefaultPasswordValidator();
     }
 
     @Test
-    void validate() {
+    void validate_ValidPassword_ReturnsTrue() {
+        String validPassword = "$Qwertyuiopasdfghjklz123";
+        when(passwordValidator.validate(validPassword)).thenReturn(true);
+
+        boolean result = defaultPasswordValidator.validate(validPassword);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void validate_InvalidPassword_ReturnsFalse() {
+        String invalidPassword = "WeakPassword";
+        when(passwordValidator.validate(invalidPassword)).thenReturn(false);
+
+        boolean result = defaultPasswordValidator.validate(invalidPassword);
+
+        assertFalse(result);
     }
 }
